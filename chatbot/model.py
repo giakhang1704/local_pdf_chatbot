f25d64bd87f70e595444091d1b9d7e1c448f7546
KhangVu <giakhang1704@gmail.com>
2025-07-17 21:13:28 +0700

    first commit

--- chatbot/model.py
+++ chatbot/model.py
@@ -1,12 +1,6 @@
-# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-# .\chatbot\Scripts\Activate.ps1
-
 from langchain_ollama import OllamaLLM
-#from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-import os
 
-#os.environ["OPENAI_API_KEY"] = "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"
 
 template = """
 Answer the question below.
